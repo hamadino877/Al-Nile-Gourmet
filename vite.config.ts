57148c77cce46e9aec62ff4cb4e932156f5387c4
Hamdy El-Gayar <hhaammddyy20001000@gmail.com>
2025-12-05 07:29:47 +0400

    Use API_KEY from environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
-  const apiKey = env.API_KEY || 'AIzaSyDwvjZlcsfdOxp4DkY-QR5Dzih9vcH1j5c';
 
   return {
     plugins: [react()],
     base: './',
     define: {
-      'process.env.API_KEY': JSON.stringify(apiKey),
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
     build: {
       chunkSizeWarningLimit: 1600,
