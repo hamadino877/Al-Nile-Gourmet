6b7eb2f4fc7df2ae1db9eefa3e1d485ea5b5eb64
Hamdy El-Gayar <hhaammddyy20001000@gmail.com>
2025-12-05 07:10:10 +0400

    Use fallback API key in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
+  const apiKey = env.API_KEY || 'AIzaSyDwvjZlcsfdOxp4DkY-QR5Dzih9vcH1j5c';
 
   return {
     plugins: [react()],
     base: './',
     define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+      'process.env.API_KEY': JSON.stringify(apiKey),
     },
     build: {
       chunkSizeWarningLimit: 1600,
